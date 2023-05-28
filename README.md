97a075425e8cffe4fea564bff8344de515e6d0d5
vskolegov <sevako011@bk.ru>
2023-05-29 00:55:19 +0300

    local impl

---
+++ README.md
@@ -0,0 +1,21 @@
+# ptztrack_ue5
+Using onvif-based ptz cameras for virtual production in Unreal Engine 5
+
+## Usage
+
+```bash
+python3 main.py
+```
+
+This is a desktop application, so you can see the interface.
+
+## Dependencies
+
+To install the dependencies, run:
+
+```bash
+pip3 install -r requirements.txt
+```
+
+## 
+This version support only local work with server
---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from frontend.controllers import PTZCameras
+from frontend.windows import MainWindow
+
+#from backend.connector import *
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow(PTZCameras())
+    window.resize(1000, 600)
+    window.setStyleSheet(open('new_frontend/styles/main.qss').read())
+    window.show()
+    sys.exit(app.exec())
